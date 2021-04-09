129a14009a038932585d1ba6c9b61f55b2252666
suke <2568597007@qq.com>
2021-04-09 10:17:01 +0800

    Initial Commit

---
+++ iMenu/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iMenu
+//
+//  Created by  suke on 2021/4/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
